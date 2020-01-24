ac68dd59cc328d92e38a35c070ae5dd3dd6bbe3e
Vineet Kishore <vineetkishore01@gmail.com>
2020-01-24 20:36:39 +0530

    Improved the app launch logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,20 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:talking_pigeon_x/chatscreen.dart';
 import 'splashscreen.dart';
 
-void main() {
-  Talkingpigeon talkingpigeon = Talkingpigeon();
-  talkingpigeon.infoAvailable().then((val) {
-    if (val == "") {
-      runApp(SplashScreen());
-    } else {
-      runApp(ChatScreenBuildContext(
-        username: val,
-      ));
-    }
-  });
-}
-
-class Talkingpigeon extends StatelessWidget {
-  Future<String> infoAvailable() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String _username;
-    _username = (prefs.getString('username') ?? '');
-    String _password = (prefs.getString('password') ?? '');
-    if (_username != '' && _password != '') {
-      //Saving credentials like username and/or password only if previous signin/signup was successful.
-      return _username;
-      //   await Firestore.instance
-      //       .document("Users/$_username")
-      //       .get()
-      //       .then((snapshot) {
-      //     if (snapshot.exists) {
-      //       userData.email = snapshot.data['email'];
-      //       userData.password = _password;
-      //       userData.uid = _username;
-      //     }
-      //   });
-      //   try {
-      //     _uid = await userAuth.verifyuser(userData);
-      //     if (_uid != null) {
-      //       setState(() {
-      //         credentialCorrectness = true;
-      //       });
-      //     }
-      //   } catch (e) {
-      //     throw e;
-      //   }
-      // } else {
-      //   setState(() {
-      //     credentialCorrectness = false;
-      //   });
-      // }
-    } else {
-      return "";
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+Future<Null> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String _username = (prefs.getString('username') ?? '');
+  print(_username);
+  runApp(MaterialApp(
+      home: _username != ""
+          ? ChatScreenBuildContext(
+              username: _username,
+            )
+          : SplashScreen()));
 }
 
 class ChatScreenBuildContext extends StatelessWidget {
@@ -66,10 +22,8 @@ class ChatScreenBuildContext extends StatelessWidget {
   ChatScreenBuildContext({this.username});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ChatScreen(
-        username: username,
-      ),
+    return ChatScreen(
+      username: username,
     );
   }
 }
